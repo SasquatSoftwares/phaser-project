865d6e74473afca19cab7b578a3004b176f4ff14
GustavoPerezYSSY <gustavo.perez@yssy.com.br>
2022-01-25 10:41:59 -0300

    fix/mixes

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -3,38 +3,42 @@ const CopyWebpackPlugin = require('copy-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-  entry: ['./src/scripts/game.ts'],
-  output: {
-    path: path.resolve(__dirname, '../dist'),
-    filename: '[name].bundle.js',
-    chunkFilename: '[name].chunk.js'
-  },
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js']
-  },
-  module: {
-    rules: [{ test: /\.tsx?$|\.jsx?$/, include: path.join(__dirname, '../src'), loader: 'ts-loader' }]
-  },
-  optimization: {
-    splitChunks: {
-      cacheGroups: {
-        commons: {
-          test: /[\\/]node_modules[\\/]/,
-          name: 'vendors',
-          chunks: 'all',
-          filename: '[name].bundle.js'
+    entry: ['./src/scripts/game.ts'],
+    devServer: {
+        port: 8008
+    },
+    output: {
+        path: path.resolve(__dirname, '../dist'),
+        filename: '[name].bundle.js',
+        chunkFilename: '[name].chunk.js'
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js']
+    },
+    module: {
+        rules: [{ test: /\.tsx?$|\.jsx?$/, include: path.join(__dirname, '../src'), loader: 'ts-loader' }]
+    },
+    optimization: {
+        splitChunks: {
+            cacheGroups: {
+                commons: {
+                    test: /[\\/]node_modules[\\/]/,
+                    name: 'vendors',
+                    chunks: 'all',
+                    filename: '[name].bundle.js'
+                }
+            }
         }
-      }
-    }
-  },
-  plugins: [
-    new HtmlWebpackPlugin({ gameName: 'My Phaser Game', template: 'src/index.html' }),
-    new CopyWebpackPlugin({
-      patterns: [
-        { from: 'src/assets', to: 'assets' },
-        { from: 'pwa', to: '' },
-        { from: 'src/favicon.ico', to: '' }
-      ]
-    })
-  ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({ gameName: 'Yssy - Tiled Virtual Reality', template: 'src/index.html' }),
+        new CopyWebpackPlugin({
+            patterns: [
+                { from: 'src/assets', to: 'assets' },
+                { from: 'pwa', to: '' },
+                { from: 'src/favicon.ico', to: '' },
+                { from: 'src/favicon.png', to: '' }
+            ]
+        })
+    ]
 }
